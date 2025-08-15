4b93c3fc723dd0ed87f690c497420b0321329864
UzWebDeveloper <max220700@gmail.com>
2025-08-15 21:48:05 +0500

    add changes

--- src/pages/internships/view/api/index.ts
+++
@@ -1,7 +0,0 @@
-import clientApi from "@/shared/api/base-api.ts"
-import { apiKeys } from "@/shared/constants/api-keys.ts"
-
-export const getInternshipApi = async (id?: number) => {
-	const response = await clientApi.get(`${apiKeys.accountApplications}/${id}`)
-	return response?.data
-}
